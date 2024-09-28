c33bde826763d2a0b31e202ff74f5bfcfe3d0b01
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-09-28 18:10:55 +0900

    improve output

--- src/editor.rs
+++ src/editor.rs
@@ -1,4 +1,4 @@
-use std::io::{self, Read};
+use crossterm::event::{read, Event::Key, KeyCode::Char};
 use crossterm::terminal::enable_raw_mode;
 use crossterm::terminal::disable_raw_mode;
 
@@ -12,21 +12,20 @@ impl Editor {
     }
     pub fn run(&self) {
         enable_raw_mode().unwrap();
-        for b in io::stdin().bytes() {
-            match b {
-                Ok(b) => {
-                    let c = b as char;
-                    if c.is_control() {
-                        println!("Binary: {0:08b} ASCII: {0:#03} \r",b);
-                    } else {
-                        println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r",b, c);
+        loop {
+            match read() {
+                Ok(Key(event)) => {
+                    println!("{:?} \r", event);
+                    match event.code {
+                        Char(c) => {
+                            if c == 'q' {
+                                break;
+                            }
+                        },
+                        _ => (),
                     }
-                    if c == 'q' {
-                        disable_raw_mode().unwrap();
-                        break;
-                    }
-                }
-                Err(err) => println!("Error: {}", err),
+            }, Err(err) => println!("Error: {}", err),
+                _ => ()
             }
         }
         disable_raw_mode().unwrap();
